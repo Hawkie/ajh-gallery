e5fceed6c6ca110fb8242d1db77f2a637ff98be6
Hawkie <paulhawkins123@gmail.com>
2019-01-09 10:43:24 +0000

    add another config

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,7 +3,8 @@ import { Component } from "react";
 
 export class Contact extends Component {
   public render(): React.ReactNode {
-    const em: string|undefined = process.env.NODE_ENV;
+    const email: string|undefined = process.env.EMAIL;
+    const env: string|undefined = process.env.NODE_ENV;
     return (
       <div className="contact">
         <h2>Where to find AJH art.</h2>
@@ -15,8 +16,8 @@ export class Contact extends Component {
         <p>Art query email: <a href="mailto:hawkinsartwork2019@gmail.com?Subject=AJH%20Art%20Query">
         hawkinsartwork2019@gmail.com</a></p>
         <p>Gallery admin email: <a href="mailto:paulhawkins123@gmail.com?Subject=AJH%20Admin%20Query">
-        paulhawkins123@gmail.com</a></p>
-        <p>env: {em}</p>
+        {email}</a></p>
+        <p>env: {env}</p>
       </div>
     );
   }
--- src/functions/test.js
+++ src/functions/test.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 exports.handler = async(event, context) => {
     const name = event.queryStringParameters.name || "World (default)";
