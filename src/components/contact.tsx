b16f4f4649eea3ce9e7aa84b87c38fb50d54fe0e
Hawkie <paulhawkins123@gmail.com>
2019-01-09 13:12:56 +0000

    setup netlify build lambda

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,3 +1,4 @@
+// import fetch from "node";
 import * as React from "react";
 import { Component } from "react";
 
@@ -5,6 +6,7 @@ export class Contact extends Component {
   public render(): React.ReactNode {
     const email: string|undefined = process.env.EMAIL;
     const env: string|undefined = process.env.NODE_ENV;
+    // const t: string = test();
     return (
       <div className="contact">
         <h2>Where to find AJH art.</h2>
@@ -17,10 +19,24 @@ export class Contact extends Component {
         hawkinsartwork2019@gmail.com</a></p>
         <p>Gallery admin email: <a href="mailto:paulhawkins123@gmail.com?Subject=AJH%20Admin%20Query">
         {email}</a></p>
-        <p>env: {env}</p>
+        <p>env: {}</p>
       </div>
     );
   }
 }
+
+// function test(): any {
+//   const ri: RequestInfo = "/.netlify/functions/test";
+//   const riDev: RequestInfo = "http://localhost:9003/test";
+//   return fetch(riDev, {
+//     method: "GET",
+//   })
+//   .then((value: Response) => {
+//     return value.json();
+//   })
+//   .then((v: any) => {
+//     return JSON.stringify(v);
+//   });
+// }
 
 export default Contact;
